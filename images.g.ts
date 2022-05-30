84338602652e17d69077a588914d522385e5a2fe
rogerrvila <106530259+rogerrvila@users.noreply.github.com>
2022-05-30 13:28:53 +0200

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Tiburón":return img`
+...........fffffff...66fff..........
+..........f8888888ff68888f..........
+..........f8811188888ff8f...........
+..........f811111ff8888ff...........
+..........f166661ff888886ff.........
+..........ff616161886868666f........
+...........f6622218886868666f..66666
+............62226188868686666f6dd886
+............622261888888866666dd866.
+............6222611888886666668866..
+...........662216118888666666f866f..
+...........6612611688866666886f66f..
+...........61111116888fddddd6.f886f.
+............661111f8d88fddd6...f88f.
+..............666fff8d88f66.....f88f
+....................fffff........fff
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
